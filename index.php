4aab3430ab84605dcc77077154c4063cf2604e37
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-10-08 00:11:53 +0200

    scss

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
+<?php include "db.php" ; ?>
+<?php echo($database); ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Discs</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
     <link rel="stylesheet" href="dist/app.css">
   </head>
   <body>
-    ciao
+    <header>
+
+    </header>
+
+    <main>
+      <div class="disc-wrapper">
+        <ul class="disc-list">
+          <li class="disc">
+            <img src="" alt="">
+            <h4></h4>
+            <p></p>
+            <p></p>
+          </li>
+        </ul>
+      </div>
+    </main>
   </body>
 </html>
