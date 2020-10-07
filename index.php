d8bd52f5a7c4e9afa9a40c981277c6971e20b02e
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-10-07 16:03:54 +0200

    creati file setup

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Discs</title>
+    <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+    ciao
+  </body>
+</html>
